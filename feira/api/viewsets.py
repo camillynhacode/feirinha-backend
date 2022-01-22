be048238d3b0e73b6f631c7b4c7ceff045105188
camillynhacode <camynunespatricio@gmail.com>
2022-01-22 14:47:21 -0300

    feat: adicionado verificação para listar apenas itens relacionado ao usuário logado

--- feira/api/viewsets.py
+++ feira/api/viewsets.py
@@ -1,13 +1,27 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 from feira.api.serializers import FeiraSerializer, ItemFeiraSerializer
-
 from feira.models import Feira, ItemFeira
 
 class FeiraviewSet(ModelViewSet):
+    permission_classes = (IsAuthenticated,)
     queryset=Feira.objects.all()
     serializer_class=FeiraSerializer
 
+    def list(self, request):
+        queryset = self.queryset.filter(usuario=request.user)
+        serializer = FeiraSerializer(queryset, many=True)
+        return Response(serializer.data)
+
 class ItenFeiraviewSet(ModelViewSet):
+    permission_classes = (IsAuthenticated,)
+
     queryset=ItemFeira.objects.all()
     serializer_class=ItemFeiraSerializer
+
+    def list(self, request):
+        queryset = self.queryset.filter(feira__usuario=request.user)
+        serializer = ItemFeiraSerializer(queryset, many=True)
+        return Response(serializer.data)
 
